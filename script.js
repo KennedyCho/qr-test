0d4fa58ca0f819b5632a1ad8cc4e7db1d5f562c2
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-11-30 07:02:36 -1000

    final

--- script.js
+++ script.js
@@ -1,12 +1,58 @@
-const express = require('express')
-const app = express()
-const port = 3000
-
-app.get('/', (req, res) => {
-res.send('Hello World!')
-})
-
-app.listen(port, () => {
-console.log(`Example app listening at http://localhost:${port}`)
-})
-
+console.log("I ran");
+$('.button').click(function(){
+    var $btn = $(this),
+        $step = $btn.parents('.modal-body'),
+        stepIndex = $step.index(),
+        $pag = $('.modal-header span').eq(stepIndex);
+  
+    if(stepIndex === 0 || stepIndex === 1) { step1($step, $pag); }
+    else { step3($step, $pag); }
+    
+  });
+  
+  
+  function step1($step, $pag){
+  console.log('step1');
+    // animate the step out
+    $step.addClass('animate-out');
+    
+    // animate the step in
+    setTimeout(function(){
+      $step.removeClass('animate-out is-showing')
+           .next().addClass('animate-in');
+      $pag.removeClass('is-active')
+            .next().addClass('is-active');
+    }, 600);
+    
+    // after the animation, adjust the classes
+    setTimeout(function(){
+      $step.next().removeClass('animate-in')
+            .addClass('is-showing');
+      
+    }, 1200);
+  }
+  
+  
+  function step3($step, $pag){
+  console.log('3');
+  
+    // animate the step out
+    $step.parents('.modal-wrap').addClass('animate-up');
+  
+    setTimeout(function(){
+      $('.rerun-button').css('display', 'inline-block');
+    }, 300);
+  }
+  
+  $('.rerun-button').click(function(){
+   $('.modal-wrap').removeClass('animate-up')
+                    .find('.modal-body')
+                    .first().addClass('is-showing')
+                    .siblings().removeClass('is-showing');
+  
+    $('.modal-header span').first().addClass('is-active')
+                            .siblings().removeClass('is-active');
+   $(this).hide();
+  });
+  
+  
